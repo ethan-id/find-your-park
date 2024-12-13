6b059ff990d68fd6e59d670dd483a21bf843aa99
ehancock <ehancock@iastate.edu>
2024-12-12 21:06:08 -0600

    Get maps pkg working kinda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-    return <div></div>;
-}
+'use client';
+
+import {FunctionComponent, useEffect, useState} from 'react';
+import {Map, APIProvider} from '@vis.gl/react-google-maps';
+
+const Home: FunctionComponent = () => {
+    const [apiKey, setApiKey] = useState<string>('');
+
+    useEffect(() => {
+        setApiKey(process.env.GOOGLE_MAPS_API_KEY as string);
+    }, [process.env.GOOGLE_MAPS_API_KEY]);
+
+    return (
+        <APIProvider apiKey={apiKey}>
+            <Map
+                style={{width: '100vw', height: '100vh'}}
+                defaultCenter={{lat: 22.54992, lng: 0}}
+                defaultZoom={3}
+                gestureHandling={'greedy'}
+                disableDefaultUI={true}
+            />
+        </APIProvider>
+    );
+};
+
+export default Home;
