a47eac1b8d3bf28926b57c6f36f07fdd42809b45
ehancock <ehancock@iastate.edu>
2024-12-15 16:01:49 -0600

    Add <InfoWindow/>'s to markers

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
-        domains: ['utfs.io']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                port: '',
+                pathname: '**',
+                search: ''
+            }
+        ]
     }
 };
 
--- src/components/markers.tsx
+++ src/components/markers.tsx
@@ -1,5 +1,5 @@
-import {AdvancedMarker, Pin} from '@vis.gl/react-google-maps';
-import {FunctionComponent} from 'react';
+import {AdvancedMarker, InfoWindow, useAdvancedMarkerRef} from '@vis.gl/react-google-maps';
+import {FunctionComponent, useState, useCallback} from 'react';
 import Image from 'next/image';
 import type {MarkerData} from '@/types/location-types';
 
@@ -11,15 +11,48 @@ export const Markers: FunctionComponent<MarkersProps> = ({markers}) => {
     return (
         <>
             {markers.map((marker) => (
-                <AdvancedMarker key={marker.park.id} position={marker.location}>
-                    <Image
-                        src={'https://utfs.io/f/vWKtdZl81f5UMw786LSheYiJSk1D7b5FHUv9Oo62BQNZLAIs'}
-                        alt={'Cat'}
-                        width={30}
-                        height={30}
-                    />
-                </AdvancedMarker>
+                <MarkerWithInfoWindow key={marker.park.id} markerData={marker} />
             ))}
+        </>
+    );
+};
+
+interface MarkerWithInfoWindowProps {
+    markerData: MarkerData;
+}
+
+const MarkerWithInfoWindow: FunctionComponent<MarkerWithInfoWindowProps> = ({markerData}) => {
+    const [markerRef, marker] = useAdvancedMarkerRef();
+    const [infoWindowShown, setInfoWindowShown] = useState(false);
+
+    const handleMarkerClick = useCallback(() => setInfoWindowShown((isShown) => !isShown), []);
+    const handleClose = useCallback(() => setInfoWindowShown(false), []);
+
+    return (
+        <>
+            <AdvancedMarker
+                ref={markerRef}
+                key={markerData.park.id}
+                position={markerData.location}
+                onClick={handleMarkerClick}
+            >
+                <Image
+                    src={'https://utfs.io/f/vWKtdZl81f5UMw786LSheYiJSk1D7b5FHUv9Oo62BQNZLAIs'}
+                    alt={'Cat'}
+                    width={30}
+                    height={30}
+                />
+            </AdvancedMarker>
+
+            {infoWindowShown && (
+                <div className='bg-slate-900'>
+                    <InfoWindow anchor={marker} onClose={handleClose} headerDisabled={true}>
+                        <div className='flex flex-col justify-center items-center text-black font-bold'>
+                            <h2>{markerData.park.fullName}</h2>
+                        </div>
+                    </InfoWindow>
+                </div>
+            )}
         </>
     );
 };
