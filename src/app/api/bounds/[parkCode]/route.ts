5d93c3fb4545d2dd0c88a1e544d05a75e57edee1
ehancock <ehancock@iastate.edu>
2025-01-23 20:01:02 -0600

    Update route

--- src/app/api/bounds/[parkCode]/route.ts
+++ src/app/api/bounds/[parkCode]/route.ts
@@ -1,6 +1,6 @@
 import {NextResponse} from 'next/server';
 
-export async function GET({params}: {params: {parkCode: string}}) {
+export async function GET(request: Request, {params}: {params: {parkCode: string}}) {
     const {parkCode} = params;
 
     try {
